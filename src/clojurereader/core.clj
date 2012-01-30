e7d27a0973cd1b13954d7b7fec00d799993de78e
Cosmin Stejerean <cosmin@offbytwo.com>
2012-01-30 15:48:25 +1100

    creating initial repository

---
+++ src/clojurereader/core.clj
@@ -0,0 +1,1 @@
+(ns clojurereader.core)
---
+++ test/clojurereader/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojurereader.test.core
+  (:use [clojurereader.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
