297c8a2de41c94f53f715d8d35c4c9a1022e0bee
Cosmin Stejerean <cosmin@offbytwo.com>
2012-02-02 21:44:15 +1100

    implemented reading symbols, and added tests for numbers and symbols

--- src/clojurereader/core.clj
+++ src/clojurereader/core.clj
@@ -1,23 +1,85 @@
 (ns clojurereader.core
+  (:refer-clojure :exclude [read read-string])
   (:use clojurereader.util
         [clojurereader.numbers :only (match-number)])
   (:import [java.io PushbackReader StringReader]
            [java.util.regex Pattern Matcher]
+           [clojure.lang Namespace Compiler Symbol Keyword]
            [clojure.lang LineNumberingPushbackReader LispReader$ReaderException]))
 
 
+(defn get-macro [ch]
+  (fn [^PushbackReader stream, Character ch] ch))
+
 (defn macro? [ch]
+  false)
+
+(defn terminating-macro? [ch]
   false)
 
 (defn whitespace?
   "Determine if the character is considered whitespace in Clojure"
   [ch] (or (Character/isWhitespace ch) (= \, ch)))
 
-(defn read-token [stream ch]
-  ch)
+(defn read-token [^PushbackReader stream, ch]
+  (let [sb (StringBuilder.)]
+    (.append sb (char ch))
+    (loop [ch (.read stream)]
+      (if (not (or (= -1 ch) (whitespace? (char ch)) (terminating-macro? ch)))
+        (do
+          (.append sb (char ch))
+          (recur (.read stream)))
+        (do
+          (.unread stream ch)
+          (.toString sb))))))
+
+(defn namespace-for
+  ([^Symbol sym] (namespace-for *ns* sym))
+  ([^Namespace inns, ^Symbol sym]
+     (let [ns-sym (Symbol/intern (.ns sym))
+           ns (.lookupAlias inns ns-sym )]
+       (if (nil? ns)
+         (Namespace/find ns-sym)
+         ns))))
+
+(def symbol-pattern (Pattern/compile "[:]?([\\D&&[^/]].*/)?([\\D&&[^/]][^/]*)"))
+(defn match-symbol [s]
+  (let [m (.matcher symbol-pattern s)]
+    (if (.matches m)
+      (let [gc (.groupCount m)
+            ns (.group m 1)
+            name (.group m 2)]
+        (cond
+         (or (and (not-nil? ns) (.endsWith ns ":/"))
+             (.endsWith name ":")
+             (not= -1 (.indexOf s "::" 1)))
+         nil
+
+         (.startsWith s "::")
+         (let [ks (Symbol/intern (.substring s 2))
+               kns (if (not-nil? (.getNamespace ks))
+                     (namespace-for ks)
+                     *ns*)]
+           (if (not-nil? kns)
+             (Keyword/intern (.. kns getName getName) (. ks getName))))
+
+         :else
+         (let [keyword? (= \: (.charAt s 0))
+               sym (Symbol/intern (.substring s (if keyword? 1 0)))]
+           (if keyword?
+             (Keyword/intern sym)
+             sym)))))))
 
 (defn interpret-token [token]
-  token)
+  (condp = token
+    "nil" nil
+    "true" true
+    "false" false
+    "/" /
+    "clojure.core//" clojure.core//
+    (if-let [matched (match-symbol token)]
+      matched
+      (throw (RuntimeException. (str "Invalid token: " token))))))
 
 (defn read-a-number [^PushbackReader stream, ^Character ch]
   (let [sb (StringBuilder.)]
@@ -58,6 +120,7 @@
                         (throw (RuntimeException. "EOF while reading"))
                         eof-value)
             (Character/isDigit ch) (read-a-number stream ch)
+            (macro? ch) ((get-macro ch) stream (char ch))
             (plus-or-minus? ch) (let [ch2 (.read stream)]
                                   (if (Character/isDigit ch2)
                                     (do
--- test/clojurereader/test/core.clj
+++ test/clojurereader/test/core.clj
@@ -1,6 +1,33 @@
 (ns clojurereader.test.core
-  (:use [clojurereader.core])
+  (:require [clojurereader.core :as cr])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest read-integers
+  (is 23 (cr/read-string "23"))
+  (is -23 (cr/read-string "-23"))
+  (is +23 (cr/read-string "+23"))
+  
+  (is 23N (cr/read-string "23N"))
+  (is -23N (cr/read-string "-23N"))
+  (is +23N (cr/read-string "+23N"))
+
+  (is 0123 (cr/read-string "0123"))
+  (is 0xff (cr/read-string "0xff")))
+
+(deftest read-floats
+  (is 2.3 (cr/read-string "2.3"))
+  (is -2.3 (cr/read-string "-2.3"))
+  (is +2.3 (cr/read-string "+2.3"))
+
+  (is 2.3M (cr/read-string "2.3M"))
+  (is -2.3M (cr/read-string "-2.3M"))
+  (is +2.3M (cr/read-string "+2.3M")))
+
+(deftest read-rations
+  (is 2/5 (cr/read-string "2/5"))
+  (is -2/5 (cr/read-string "-2/5")))
+
+(deftest read-symbols
+  (is :foo (cr/read-string ":foo"))
+  (is :clojurereader.test.core/foo (cr/read-string "::foo"))
+  (is 'clojure.core/map (cr/read-string "clojure.core/map")))
