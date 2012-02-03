47820e5d9ed542bbb1aa90d702917a548c2d5dc0
Cosmin Stejerean <cosmin@offbytwo.com>
2012-02-03 23:37:42 +1100

    renaming tests

--- test/clojure_reader/test/core.clj
+++ test/clojure_reader/test/core.clj
@@ -2,7 +2,7 @@
   (:require [clojure-reader.core :as cr])
   (:use [clojure.test]))
 
-(deftest read-integers
+(deftest reading-integers
   (is 23 (cr/read-string "23"))
   (is -23 (cr/read-string "-23"))
   (is +23 (cr/read-string "+23"))
@@ -14,7 +14,7 @@
   (is 0123 (cr/read-string "0123"))
   (is 0xff (cr/read-string "0xff")))
 
-(deftest read-floats
+(deftest reading-floats
   (is 2.3 (cr/read-string "2.3"))
   (is -2.3 (cr/read-string "-2.3"))
   (is +2.3 (cr/read-string "+2.3"))
@@ -23,37 +23,37 @@
   (is -2.3M (cr/read-string "-2.3M"))
   (is +2.3M (cr/read-string "+2.3M")))
 
-(deftest read-rations
+(deftest reading-rations
   (is 2/5 (cr/read-string "2/5"))
   (is -2/5 (cr/read-string "-2/5")))
 
-(deftest read-symbols
+(deftest reading-symbols
   (is :foo (cr/read-string ":foo"))
   (is :clojure-reader.test.core/foo (cr/read-string "::foo"))
   (is 'clojure.core/map (cr/read-string "clojure.core/map")))
 
-(deftest read-regex
+(deftest reading-regexs
   (is #"foo" (cr/read-string "#\"foo\"")))
 
-(deftest read-list
+(deftest reading-lists
   (is '() (cr/read-string "()"))
   (is '(1) (cr/read-string "(1)"))
   (is '(1 2 3) (cr/read-string "(1 2 3)"))
   (is '(1 (2 3)) (cr/read-string "(1 (2 3))")))
 
-(deftest read-vector
+(deftest reading-vectors
   (is [] (cr/read-string "[]"))
   (is [1] (cr/read-string "[1]"))
   (is [1 2 3] (cr/read-string "[1 2 3]"))
   (is [1 [2 3]] (cr/read-string "[1 [2 3]]")))
 
-(deftest read-map
+(deftest reading-maps
   (is {} (cr/read-string "{}"))
   (is {1 2} (cr/read-string "{1 2}"))
   (is {1 {2 3}} (cr/read-string "{1 {2 3}}"))
   (is {1 {2 3} 4 5} (cr/read-string "{1 {2 3} 4 5}")))
 
-(deftest read-set
+(deftest reading-sets
   (is #{} (cr/read-string "#{}"))
   (is #{1} (cr/read-string "#{1}"))
   (is #{1 2} (cr/read-string "#{1 2}"))
@@ -64,7 +64,7 @@
   (is {1 2} (cr/read-string "{1, 2}"))
   (is 1 (cr/read-string ",1")))
 
-(deftest read-metadata
+(deftest reading-metadata
   (is {:foo true} (meta (cr/read-string "^:foo (1 2 3)")))
   (is {:a 1} (meta (cr/read-string "^{:a 1} (1 2 3)")))
   )
