dfe07bf6ced3cd43b6cd82f663429e71ea331a8c
Cosmin Stejerean <cosmin@offbytwo.com>
2012-02-15 14:36:02 +1100

    renamed tests

--- test/clojure_reader/test/core.clj
+++ test/clojure_reader/test/core.clj
@@ -2,9 +2,9 @@
   (:require [clojure-reader.core :as cr])
   (:use [clojure.test]))
 
-(deftest reading-integers
+(deftest test-reading-integers
   (is (= 23 (cr/read-string "23")))
-  (is (=  -23 (cr/read-string "-23")))
+  (is (= -23 (cr/read-string "-23")))
   (is (= +23 (cr/read-string "+23")))
   
   (is (= 23N (cr/read-string "23N")))
@@ -14,7 +14,7 @@
   (is (= 0123 (cr/read-string "0123")))
   (is (= 0xff (cr/read-string "0xff"))))
 
-(deftest reading-floats
+(deftest test-reading-floats
   (is (= 2.3 (cr/read-string "2.3")))
   (is (= -2.3 (cr/read-string "-2.3")))
   (is (= +2.3 (cr/read-string "+2.3")))
@@ -23,11 +23,11 @@
   (is (= -2.3M (cr/read-string "-2.3M")))
   (is (= +2.3M (cr/read-string "+2.3M"))))
 
-(deftest reading-rations
+(deftest test-reading-rations
   (is (= 2/5 (cr/read-string "2/5")))
   (is (= -2/5 (cr/read-string "-2/5"))))
 
-(deftest reading-symbols
+(deftest test-reading-symbols
   (is (= :foo (cr/read-string ":foo")))
   (is (= :user/foo (cr/read-string "::foo")))
   (is (= 'clojure.core/map (cr/read-string "clojure.core/map"))))
@@ -36,43 +36,43 @@
   (and (= (class r1) (class r2))
        (= (str r1) (str r2))))
 
-(deftest reading-regexs
+(deftest test-reading-regexs
   (is (regex-equal #"foo" (cr/read-string "#\"foo\""))))
 
-(deftest reading-lists
+(deftest test=reading-lists
   (is (= '() (cr/read-string "()")))
   (is (= '(1) (cr/read-string "(1)")))
   (is (= '(1 2 3) (cr/read-string "(1 2 3)")))
   (is (= '(1 (2 3)) (cr/read-string "(1 (2 3))"))))
 
-(deftest reading-vectors
+(deftest test-reading-vectors
   (is (= [] (cr/read-string "[]")))
   (is (= [1] (cr/read-string "[1]")))
   (is (= [1 2 3] (cr/read-string "[1 2 3]")))
   (is (= [1 [2 3]] (cr/read-string "[1 [2 3]]"))))
 
-(deftest reading-maps
+(deftest test-reading-maps
   (is {} (cr/read-string "{}"))
   (is {1 2} (cr/read-string "{1 2}"))
   (is {1 {2 3}} (cr/read-string "{1 {2 3}}"))
   (is {1 {2 3} 4 5} (cr/read-string "{1 {2 3} 4 5}")))
 
-(deftest reading-sets
+(deftest test-reading-sets
   (is (= #{} (cr/read-string "#{}")))
   (is (= #{1} (cr/read-string "#{1}")))
   (is (= #{1 2} (cr/read-string "#{1 2}")))
   (is (= #{1 2 #{3 4}} (cr/read-string "#{1 2 #{3 4}}"))))
 
-(deftest comma-is-whitespace
+(deftest test-comma-is-whitespace
   (is (= [1 2] (cr/read-string "[1, 2]")))
   (is (= {1 2} (cr/read-string "{1, 2}")))
   (is (= 1 (cr/read-string ",1"))))
 
-(deftest reading-metadata
+(deftest test-reading-metadata
   (is (= {:foo true} (meta (cr/read-string "^:foo (1 2 3)"))))
   (is (= {:a 1} (meta (cr/read-string "^{:a 1} (1 2 3)")))))
 
-(deftest reading-strings
+(deftest test-reading-strings
   (let [simple-string "\"abcd\""
         octal-string "\"abc\\177\""
         unicode-string "\"abc\\0104\""
@@ -86,7 +86,7 @@
     (is (= (clojure.core/read-string escaped-string)
            (cr/read-string escaped-string)))))
 
-(deftest read-characters
+(deftest test-read-characters
   (is (= \space (cr/read-string "\\space")))
   (is (= \c     (cr/read-string "\\c")))
   (is (= \o377  (cr/read-string "\\o377")))
